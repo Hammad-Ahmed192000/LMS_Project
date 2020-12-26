f0c17dffc765f5011850769cb1e341fee1b45a79
MAkram1425 <makram.bcss19@iba-suk.edu.pk>
2020-12-26 17:43:13 +0500

    Modication in StudentProfile.java class with JDBC (Java Database Connectivity..)

--- StudentProfile.java
+++ StudentProfile.java
@@ -1,35 +1,44 @@
-
+package finalProject;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 class StudentProfile extends JFrame implements ActionListener
 {
-    private JFrame frame;
+    //private JFrame frame;
     private JLabel student_pro_label,footer_label,image_label;
-    private JPanel header,footer;// center
+    private JPanel header,footer,center;
     private Font font1,font2,font3;
     private Container c;
-   // private ImageIcon imgIcon,imgIcon2;
-   // private Image img, newimg,icon;
+    // private ImageIcon imgIcon,imgIcon2;
+    // private Image img, newimg,icon;
     private JMenuBar mb;
     private JMenu home,show_books,librarian,book_issuance,help,about,logout ;
     private JMenuItem logIn, create_account,avaialabe_books,librarian_pro,books,contact_us,description_LMS,confirm,no;
+    private JScrollPane scrollPane ;
+    //JLabel label ;
+    // JTabbedPane tabbedPane ;
+    private JTable students ;
     // private Insets inset;
     // private Dimension size;
 
     StudentProfile()
     {
 
-        frame=new JFrame();
+        //frame=new JFrame();
         font1=new Font("Arial", ALLBITS, 16);
 
         font2=new Font("Matura MT Script Capitals",ALLBITS,28);
         font3=new Font("Arial",ALLBITS,14);
-        c = frame.getContentPane();
+        c.add(new JFrame().getContentPane());
         student_pro_label=new JLabel("Student Profile");
 
         student_pro_label.setFont(font2);
@@ -41,7 +50,7 @@ class StudentProfile extends JFrame implements ActionListener
         //icon = Toolkit.getDefaultToolkit().getImage("LMS.png");
 
         header=new JPanel();
-        //center=new JPanel();
+        center=new JPanel();
         footer=new JPanel();
         mb=new JMenuBar();
         home=new JMenu("Home");
@@ -63,7 +72,7 @@ class StudentProfile extends JFrame implements ActionListener
         confirm=new JMenuItem("Confirm");
         no=new JMenuItem("No");
 
-        //adding JMenuItems on JMenu objects 
+        //adding JMenuItems on JMenu objects
         home.add(logIn);
         home.add(create_account);
         show_books.add(avaialabe_books);
@@ -94,7 +103,7 @@ class StudentProfile extends JFrame implements ActionListener
                 } catch (Exception ex) {
 
                 }
-                frame.dispose();
+                dispose();
             }
         });
 
@@ -107,7 +116,7 @@ class StudentProfile extends JFrame implements ActionListener
                 } catch (Exception ex) {
 
                 }
-                frame.dispose();
+                dispose();
             }
         });
 
@@ -120,7 +129,7 @@ class StudentProfile extends JFrame implements ActionListener
                 } catch (Exception ex) {
 
                 }
-                frame.dispose();
+                dispose();
             }
         });
 
@@ -128,12 +137,12 @@ class StudentProfile extends JFrame implements ActionListener
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                   //  call show books class here ;
-                    new AboutLMS();
+                    //  call show books class here ;
+                    //new AboutLMS();
                 } catch (Exception ex) {
 
                 }
-               // frame.dispose();
+                // frame.dispose();
             }
         });
         contact_us.addActionListener(new ActionListener() {
@@ -141,11 +150,11 @@ class StudentProfile extends JFrame implements ActionListener
             public void actionPerformed(ActionEvent e) {
                 try {
                     // calll login page
-                    new AboutLMS();
+                    //new AboutLMS();
                 } catch (Exception ex) {
 
                 }
-                frame.dispose();
+                dispose();
             }
         });
         description_LMS.addActionListener(new ActionListener() {
@@ -153,11 +162,11 @@ class StudentProfile extends JFrame implements ActionListener
             public void actionPerformed(ActionEvent e) {
                 try {
                     // calll login page
-                    new AboutLMS();
+                    //new AboutLMS();
                 } catch (Exception ex) {
 
                 }
-                frame.dispose();
+                dispose();
             }
         });
         confirm.addActionListener(new ActionListener() {
@@ -165,32 +174,93 @@ class StudentProfile extends JFrame implements ActionListener
             public void actionPerformed(ActionEvent e) {
                 try {
                     // calll login page
-                    new AboutLMS();
+                    //new AboutLMS();
                 } catch (Exception ex) {
 
                 }
-                frame.dispose();
+                dispose();
             }
         });
         no.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    // calll login page
-                    new AboutLMS();
+                    // call login page
+                    //new AboutLMS();
                 } catch (Exception ex) {
 
                 }
-                frame.dispose();
+                dispose();
             }
         });
+        // For Adding table logic begin from here JDBC
+        int dim1 = 0 ;
+        String columns [] = {"student_id" , "students_name" , "program" , "semester", "batch"};
+
+        Border border = BorderFactory.createLineBorder(Color.WHITE ,3);
+ try
+ {
+     Class.forName("com.mysql.jdbc.Driver");
+
+     // here plms is MYSQL database name, root is username and maadi192000 is password
+     Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_management_system","root","maadi192000");
+     Statement stmt = con.createStatement();
+
+     //Retrieving the data from plms.students table
+     ResultSet rs = stmt.executeQuery("SELECT * FROM books;");
+
+     while(rs.next())
+     {
+         ++dim1 ;
+     }
+
+     String studentsRecords [][] = new String[dim1][5] ;
+
+     rs = stmt.executeQuery("SELECT * FROM books;");
+
+     int index = 0 ;
+     while (rs.next())
+     {
+         studentsRecords[index][0] = Integer.toString(rs.getInt(1));
+         studentsRecords[index][1] = rs.getString(2) ;
+         studentsRecords[index][2] = rs.getString(3) ;
+         studentsRecords[index][3] = rs.getString(4) ;
+         studentsRecords[index][4] = rs.getString(5) ;
+         ++index ;
+     }
+
+
+
+     students = new JTable(studentsRecords , columns) ;
+     students.setBounds(200 , 200 , 400 , 500);
+
+
+     Border border2 = BorderFactory.createLineBorder(Color.BLACK ,3);
+     scrollPane = new JScrollPane(students) ;
+     scrollPane.setSize(700,500);
+     scrollPane.setLocation(350,100);
+
+
+     scrollPane.setBorder(border2);
+
+     // scrollPane.setBounds(200 , 200 , 400 , 500);
+
+     stmt.close();
+     con.close();
+
+ }
+ catch(Exception e)
+ {
+     JOptionPane.showMessageDialog(null , e.getMessage());
+ }
+
 
         //header.add(label1);
         header.add(student_pro_label);
         header.setBackground(new Color(111,120,185));
 
-        //center.setLayout(new FlowLayout());
-       // center.add(table);
+        center.setLayout(new FlowLayout());
+        center.add(scrollPane);
 
         footer.add(footer_label);
         footer.setFont(font3);
@@ -198,31 +268,28 @@ class StudentProfile extends JFrame implements ActionListener
 
 
         c.add(header,BorderLayout.NORTH);
-        //c.add(center,BorderLayout.CENTER);
+       c.add(center,BorderLayout.CENTER);
         c.add(footer,BorderLayout.SOUTH);
         //c.setBackground(Color.gray);
 
 
         // c.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
         // c.setLayout(new GridLayout(4, 1));
-        frame.setVisible(true);
-        frame.setJMenuBar(mb);
-        //frame.setIconImage(icon);
-        //frame.pack();
-        frame.setSize(1280,720);
-        frame.setLocationRelativeTo(null);
-        //frame.setBounds(200, 100, 900, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Student Profile");
+        setVisible(true);
+        setJMenuBar(mb);
+        setSize(1280,720);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Student Profile");
 
     }
 
     //private ImageIcon createImageIcon(String iconjpg, String a_pretty_but_meaningless_splat) {
-       // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     //}
-    public static void main(String args[]){
-        new StudentProfile();
-    }
+   // public static void main(String args[]){
+       // new StudentProfile();
+   // }
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -231,11 +298,11 @@ class StudentProfile extends JFrame implements ActionListener
 
     private void actionPerformed2(ActionEvent e) {
         try {
-           // new AboutLMS();
+            // new AboutLMS();
             //JOptionPane.showMessageDialog(null,"This is Desktop Application for Library Management System");
         } catch (Exception ignored) {
 
         }
-        frame.dispose();
+        dispose();
     }
 }
