0ec30c64e8a95d325f1dc0d2ddeb7c266f355750
MAkram1425 <makram.bcss19@iba-suk.edu.pk>
2020-12-26 17:49:01 +0500

    Adding ContactUs.java class

---
+++ ContactUs.java
@@ -0,0 +1,12 @@
+
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class ContactUs{
+
+    ContactUs(){
+
+        showMessageDialog(null,"In case of any serious issue contact us through email.\n"  +
+                "M.Akram : makram.bcss19@iba-suk.edu.pk \n" +
+                "Hammad Ahmed : hammad.bcss19@iba-suk.edu.pk");
+    }
+}
