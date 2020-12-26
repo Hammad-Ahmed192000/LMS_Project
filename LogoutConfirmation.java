e3b10b591fe4be317fd3e82fd629214c1c730a49
MAkram1425 <makram.bcss19@iba-suk.edu.pk>
2020-12-26 18:01:37 +0500

    Adding LogoutConfirmation.java class

---
+++ LogoutConfirmation.java
@@ -0,0 +1,10 @@
+
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class LogoutConfirmation{
+
+    LogoutConfirmation(){
+
+        showMessageDialog(null,"Are you sure to logout");
+    }
+}
