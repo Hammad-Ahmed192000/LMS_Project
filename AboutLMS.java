d11c033b542e893d657d35aa6c45264b8928e5e4
MAkram1425 <makram.bcss19@iba-suk.edu.pk>
2020-12-26 17:47:25 +0500

    Adding AboutLMS.java class

---
+++ AboutLMS.java
@@ -0,0 +1,12 @@
+
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class AboutLMS {
+
+    AboutLMS(){
+
+        showMessageDialog(null,"this is a Desktop Application for Library Management System.\n"  +
+                "It will benifit Student to issue books from library \n" +
+                " and it will also help Librarian to keep record of each book..");
+    }
+}
