1ff58a0a4aeef5b47be3a64e30eb92a2afb1d0da
Hammad-Ahmed192000 <hammad.bcss19@iba-suk.edu.pk>
2021-01-05 20:53:30 -0800

    Final Modification in LMSProject/Main Frame.

--- LMSProject.java
+++ LMSProject.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionListener;
 
 class LMSProject extends Frame
 {
-    JSeparator separator1 , separator2 ;
+    JSeparator separator1 , separator2, separator ;
     JLabel label ;
     JLabel background ;
     ImageIcon icon ;
@@ -24,25 +24,22 @@ class LMSProject extends Frame
         label = new JLabel();
         label.setText("If you are new student you must create your account before login\n else click on Login Button");
         label.setForeground(Color.BLACK);
-        label.setBounds(475,425,500,100);
+        label.setBounds(385,425,800,100);
         label.setFont(new Font("Aerial",Font.BOLD,14));
         label.setLayout(null);
 
+        separator  = new JSeparator() ;
+        separator.setBounds(0, 120 ,1350,50 );
+        separator.setBackground(Color.BLACK);
+
+
         separator1 = new JSeparator() ;
-        separator1.setBounds(475,500,480,100);
-        separator1.setBackground(Color.WHITE);
+        separator1.setBounds(385,500,635,100);
+        separator1.setBackground(Color.BLACK);
 
         separator2 = new JSeparator() ;
-        separator2.setBounds(475,450,480,100);
-        separator2.setBackground(Color.WHITE);
-
-
-
-        // this code is for setting the BackGround.
-        icon = new ImageIcon("background.jpg") ;
-        background = new JLabel(icon) ;
-        background.setBounds(0 , 0 , 1350,850);
-        background.setVisible(true);
+        separator2.setBounds(385,450,635,100);
+        separator2.setBackground(Color.BLACK);
 
 
         l1.setBounds(375,50,700,60);
@@ -58,6 +55,7 @@ class LMSProject extends Frame
         bL.setBackground(Color.WHITE);
 
         l1.setForeground(Color.BLACK);
+        l1.setBackground(null);
         l1.setFont(new Font("Serif",Font.BOLD + Font.PLAIN,50));
         bA.setFont(new Font("Aerial",Font.BOLD,17));
         bL.setFont(new Font("Aerial",Font.BOLD,17));
@@ -65,22 +63,16 @@ class LMSProject extends Frame
         bA.addActionListener(new actions());
         bL.addActionListener(new actions());
 
-        // add(backgroud);
-        background.add(l1) ;
-        background.add(bA) ;
-        background.add(bL);
-        background.add(separator1) ;
-        background.add(label) ;
-        background.add(separator2) ;
-        add(background);
-
-        /*
-        add(l1);
-        add(bA);
+        add(l1) ;
+        add(bA) ;
         add(bL);
-        */
+        add(separator) ;
+        add(separator1) ;
+        add(label) ;
+        add(separator2) ;
+        // add(background);
 
-
+        setBackground(new Color(51,  51, 51));
         setLayout(null);
         setTitle("Library Managment System");
         setSize(1350,850);
